9ecb5e762485efb3e5cc4e24d4d1144145870921
Neelam Kushwah <neelam.kushwah@gm.com>
2024-01-11 16:49:19 -0500

    Add copyright header

--- tools/create_services_json_for_ui.py
+++ tools/create_services_json_for_ui.py
@@ -1,3 +1,29 @@
+# -------------------------------------------------------------------------
+#
+# Copyright (c) 2023 General Motors GTO LLC
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# SPDX-FileType: SOURCE
+# SPDX-FileCopyrightText: 2023 General Motors GTO LLC
+# SPDX-License-Identifier: Apache-2.0
+#
+# -------------------------------------------------------------------------
+
 import json
 import os
 import re
@@ -6,7 +32,6 @@ data = []
 
 
 def process_proto(directory):
-
     for root, dirs, files in os.walk(directory):
         service_file_path = None
         topic_file_path = None
@@ -16,19 +41,18 @@ def process_proto(directory):
             if file.endswith('_topics.proto'):
                 topic_file_path = os.path.join(root, file)
         if service_file_path and topic_file_path:
-            read_proto_file(service_file_path,topic_file_path)
+            read_proto_file(service_file_path, topic_file_path)
 
 
-def read_proto_file(service_file_path,topic_file_path):
-
+def read_proto_file(service_file_path, topic_file_path):
     with open(service_file_path, 'r') as file:
         service_file_content = file.read()
     with open(topic_file_path, 'r') as file:
         topic_file_content = file.read()
-    extract_proto_info(service_file_content,topic_file_content)
+    extract_proto_info(service_file_content, topic_file_content)
 
 
-def extract_proto_info(service_file_content,topic_file_content):
+def extract_proto_info(service_file_content, topic_file_content):
     # Extract service name
     service_name_match = re.search(r'service\s+(\w+)\s*{', service_file_content)
     service_name = service_name_match.group(1) if service_name_match else None
@@ -44,16 +68,11 @@ def extract_proto_info(service_file_content,topic_file_content):
     message_value_match = re.findall(r'message\s+(\w+)\s*{', topic_file_content)
     messages = message_value_match if message_value_match else None
 
-    append_to_data(uprotocol_name, service_name, rpcs,messages)
+    append_to_data(uprotocol_name, service_name, rpcs, messages)
 
 
-def append_to_data(entity, display_name, rpcs,messages):
-    json_obj = {
-        "name": entity,
-        "display_name": display_name,
-        "rpc": rpcs,
-        "message": messages
-    }
+def append_to_data(entity, display_name, rpcs, messages):
+    json_obj = {"name": entity, "display_name": display_name, "rpc": rpcs, "message": messages}
 
     data.append(json_obj)
 
